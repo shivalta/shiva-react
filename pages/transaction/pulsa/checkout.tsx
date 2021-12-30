b2fd3af9d48b2f1bc7d8457ed65cc359c57f1859
kopireng06 <naufalghaniachmani@gmail.com>
2021-12-30 15:50:27 +0700

    change url image provider pulsa

--- pages/transaction/pulsa/checkout.tsx
+++ pages/transaction/pulsa/checkout.tsx
@@ -19,12 +19,12 @@ const Checkout = () => {
         {
             id:"1",
             name:"BCA virtual account",
-            logo:logoBCA
+            logo:"/../../../public/images/bca-2.png"
         },
         {
             id:"2",
             name:"Mandiri virtual account",
-            logo:logoMandiri
+            logo:"/../../../public/images/mandiri-2.png"
         }
     ]
 
